f48f737fe1f6e1e2ca1fd45429da31f06f51e1af
Cristian Bourceanu <54813268+cristi-bourceanu@users.noreply.github.com>
2021-02-01 23:33:45 +0000

    Comments added

--- example.py
+++ example.py
@@ -1,15 +1,16 @@
 from OptionOrderPlacer import *
 
 if __name__ == "__main__":
+    # Configure logging format: Time + message. Truncates messages written to console to 700 characters.
     logging.basicConfig(format="%(asctime)s: %(message).700s", level=logging.INFO,
                             datefmt="%H:%M:%S")
 
     from UserCredentials import Client_Id, Client_Secret
 
-    client_signature = ClientSignature(Client_Id, Client_Secret)
-    client_websocket = ClientWebsocket(client_signature)
-    instruments = Instruments(client_websocket)
-    option_order = OptionsOrder(client_websocket, instruments=instruments)
+    client_signature = ClientSignature(Client_Id, Client_Secret)    # Create secure client signatures to log with
+    client_websocket = ClientWebsocket(client_signature)            # Instance of websocket that handles interaction with Deribit
+    instruments = Instruments(client_websocket)                     # Instance of Instruments that updates instruments every day and match the best instrument.
+    option_order = OptionsOrder(client_websocket, instruments=instruments)  # Handles querring for instruments, placing orders and refreshing orders
 
     instrument_name,_,_ = option_order.get_instrument(round(1000*(time.time()+200)), 1319.3, 'call')
     while True:
